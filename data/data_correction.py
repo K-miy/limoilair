66e2274c238862ad09462ec9ba292890b2fad8d5
K-miy <git@cbesse.net>
2022-10-05 10:25:32 -0400

    trying correction

--- data/data_correction.py
+++ data/data_correction.py
@@ -6,3 +6,11 @@ def correction_pm25(pm25:np.array, humidity:np.array) -> np.array:
 
     return 0.524 * pm25 - 0.0862 * humidity + 5.75
 
+# %%
+
+# trying things
+a = df_all['PM25 (ug/m3)'].to_numpy()
+b = df_all['Humidity (%HR)'].to_numpy()
+
+df_all['PM25 (HR-corr)'] = correction_pm25(a,b)
+# %%
